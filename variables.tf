ab2fde759470e044af5220afacb42741c70c9672
halephu01 <halephu01@gmail.com>
2024-06-21 04:15:29 +0700

    v rising

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "access_key"  {
   description = "The access key for AWS"
   type        = string
+  default = local.access_key
 }
 
 variable "secret_key" {
   description = "The secret key for AWS"
   type        = string
+  default = local.secret_key
 }
