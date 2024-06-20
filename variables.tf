8c677caf02504b88f7449531a3b669e351e32257
halephu01 <halephu01@gmail.com>
2024-06-21 04:10:20 +0700

    test again

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "access_key"  {
   description = "The access key for AWS"
   type        = string
-  default = file("./accesskey")
 }
 
 variable "secret_key" {
   description = "The secret key for AWS"
   type        = string
-  default = file("./secretkey")
 }
