12f9a92ae571fb3f7acc426c0dcb9fe78423701c
tiendat1011 <126654756+tiendat1011@users.noreply.github.com>
2024-06-24 15:04:12 +0700

    wrong syntax iam role name

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -96,7 +96,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 resource "aws_iam_role" "service_accounts" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-  name               = "Service accounts"
+  name               = "service-accounts-role"
 }
 
 # Create EKS Node Group
