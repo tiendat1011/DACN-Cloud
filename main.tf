e93b71537fe1c64f3c0c7a59ca84160c302d3f8f
tiendat1011 <126654756+tiendat1011@users.noreply.github.com>
2024-06-24 15:20:36 +0700

    add input subnets

--- main.tf
+++ main.tf
@@ -79,6 +79,8 @@ module "vpc" {
 module "eks" {
   source = "./modules/eks"
   cluster_name = "my-cluster"
+  subnet_1_id = module.vpc.subnet_1_id
+  subnet_2_id = module.vpc.subnet_2_id
   key_name = aws_key_pair.eks_keypair.key_name
   security_group_id = module.vpc.security_group_id
 }
