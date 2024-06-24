c7406e7bb00ba79f09801ffb0784c76132122545
tiendat1011 <126654756+tiendat1011@users.noreply.github.com>
2024-06-24 17:46:31 +0700

    disable kubectl apply

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -4,4 +4,6 @@ output "endpoint" {
 
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.eks.certificate_authority[0].data
+  value = aws_eks_node_group.frontend_node_group.node_group_name
+output "backend_node_group_name" {
 }
