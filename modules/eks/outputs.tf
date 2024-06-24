fb3727a62862af046533ea8a50da218b495e56d8
tiendat1011 <126654756+tiendat1011@users.noreply.github.com>
2024-06-24 17:47:32 +0700

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -4,6 +4,12 @@ output "endpoint" {
 
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.eks.certificate_authority[0].data
+}
+
+output "frontend_node_group_name" {
   value = aws_eks_node_group.frontend_node_group.node_group_name
+}
+
 output "backend_node_group_name" {
+  value = aws_eks_node_group.backend_node_group.node_group_name
 }
