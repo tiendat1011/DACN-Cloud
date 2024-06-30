b4f512328b6a84635bb0a201ebdfab23de0d784b
tiendat1011 <126654756+tiendat1011@users.noreply.github.com>
2024-06-30 18:23:33 +0700

    Update variables.tf

--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -2,7 +2,7 @@ variable "lb_tags" {
   description = "Tags to identify the Load Balancer"
   type        = map(string)
   default     = {
-    kubernetes.io/cluster/my-cluster = "owned"
-    kubernetes.io/service-name    = "ingress-nginx/ingress-nginx-controller"
+    "kubernetes.io/cluster/my-cluster" = "owned"
+    "kubernetes.io/service-name"    = "ingress-nginx/ingress-nginx-controller"
   }
 }
